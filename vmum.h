31e108f0c866da3c6a55e38831149f60d92a8f9f
Vladimir N. Makarov <vmakarov.gcc@gmail.com>
2025-01-21 09:19:03 -0500

    New version vmum.h working better with mid-length range of keys.

--- vmum.h
+++ vmum.h
@@ -51,7 +51,7 @@ typedef unsigned __int64 uint64_t;
 #include <stdint.h>
 #endif
 
-#ifdef __GNUC__
+#if defined(__GNUC__)
 #define _MUM_ATTRIBUTE_UNUSED __attribute__ ((unused))
 #define _MUM_OPTIMIZE(opts) __attribute__ ((__optimize__ (opts)))
 #define _MUM_TARGET(opts) __attribute__ ((__target__ (opts)))
@@ -76,25 +76,45 @@ typedef unsigned __int64 uint64_t;
 
 /* Here are different primes randomly generated with the equal probability of their bit values. They
    are used to randomize input values. */
-static uint64_t _mum_hash_step_prime = 0x2e0bb864e9ea7df5ULL;
-static uint64_t _mum_key_step_prime = 0xcdb32970830fcaa1ULL;
-static uint64_t _mum_block_start_prime = 0xc42b5e2e6480b23bULL;
-static uint64_t _mum_tail_prime = 0xaf47d47c99b1461bULL;
-static uint64_t _mum_finish_prime1 = 0xa9a7ae7ceff79f3fULL;
-static uint64_t _mum_finish_prime2 = 0xaf47d47c99b1461bULL;
+static uint64_t _mum_hash_step_prime = 0x7fc65e8a22c2f74bull;
+static uint64_t _mum_key_step_prime = 0x9b307d68270e94e5ull;
+static uint64_t _mum_block_start_prime = 0x6608b54dafbc797cull;
+static uint64_t _mum_tail_prime = 0xc6f58747253b0e84ull;
+static uint64_t _mum_finish_prime1 = 0xbc4bb29ce739b5d9ull;
+static uint64_t _mum_finish_prime2 = 0x7007946aa4fdb987ull;
 
-static uint64_t _mum_unroll_primes[]
-  = {0x14b09aa58cb268d5, 0xf34d372c6f503cc9, 0x6289be577a4ae0a6, 0x3ba5692b1eafeb15};
+static uint64_t _mum_unroll_primes[] = {
+  0x191fb5fc4a9bf2deull,
+  0xd9a09a0a2c4eb3ebull,
+  0x90f15ee96deb1eecull,
+  0x1a970df0a79d09baull,
+};
 
 static uint64_t _mum_primes[] = {
-  0X6413c7b09fc3dcc8, 0X55fc3da745579871, 0X160111a644959396, 0Xd12e059acb048b01,
-  0Xc66e719ac9a96714, 0X832bb4f760cb0c0d, 0X92b777a80cfd2a59, 0X883dc6537f8d2a6b,
-  0Xf40a3463bd3220d7, 0Xadd624b1439d6507, 0X2d11c5c5e6c3bf75, 0Xfc027148e7ed66c9,
-  0Xe9437c1b3494cb46, 0Xacd35405f3d2f4b7, 0X838f551f688bd046, 0X3e87440f1a4642e9,
-  0Xce20a8f09e985b88, 0Xa98931ac95adde85, 0Xe3580702cf5a1c80, 0X8e76e05a566733cd,
-  0X15ff369908c7d67d, 0X11bc44ed911d2aa7, 0Xab2694c16fe59e02, 0Xc1f5ba02d82d5e85,
-  0X7c59a06636a7eddd, 0X684643ed7e49b619, 0X40b6270f7f9fa251, 0X9d8b29eef5b17a8d,
-  0X4044ad0f26d7e774, 0Xf3d1fd1ab3ca5037, 0X5ec3d199e0063437, 0X12fb529ef8cc3a73,
+  0x0c3ec9639d3a203full, 0x898368b5fb060422ull, 0xfe3c08767c1e5068ull, 0x4535ac3cb182d3caull,
+  0xba6ba8dcc8a2d978ull, 0x9f1221df37b27ca1ull, 0x57b1b40817cde05eull, 0xadb5c6075e5dd1c3ull,
+  0x0f91abf611686bc3ull, 0x72fc850fbe9023f4ull, 0x4922ec730400d7e1ull, 0x7452d927d9970eb2ull,
+  0x607ad8c0dbe89ff9ull, 0x463bf8a99a222448ull, 0x6a0c8e5c2171d499ull, 0x88a9af1a70175e0full,
+
+  0x50c07b9351011316ull, 0xce26a1f4e66e3d1full, 0x396d79c90c86b1edull, 0x3739b72e11fc4684ull,
+  0x7fe4adc8400af08eull, 0xfa2e47475b11a3ceull, 0xd519635f1d7b9242ull, 0x866d04bd866130fcull,
+  0x6132e913fd0ae2c9ull, 0xaeacc8d99a02880dull, 0xf196fbab2ef62dbbull, 0x62a6a4ae6d3f5fddull,
+  0x3147dbedb6618cf4ull, 0x18dc2a4e6e46b7f5ull, 0x98ce62d3e346f804ull, 0x3e507b6626b9c9c6ull,
+
+  0x08d2d6aba49b53a7ull, 0x64f897cf54cc0984ull, 0xe6568e3081d4dc31ull, 0x936eff54d90e79bdull,
+  0x45ecee4cb55e5529ull, 0xe7729fe4b83bd1f0ull, 0x8ced03855b3a62aeull, 0xc29ee17b601d5ea6ull,
+  0xfadd87cd819b8b23ull, 0x5ff6b6787a02da34ull, 0xcc0f997728759ba9ull, 0x06a51956916c45ffull,
+  0xd31923282290a41full, 0x9e2fcda93feb36aaull, 0xfdc11827f9bb4b63ull, 0xe11b2c099cf17325ull,
+
+  0xd4efd1919316bb4eull, 0x7749918c7756d897ull, 0x32eff42dc1c4f41eull, 0x4171f5e422bdf4faull,
+  0xdd5c7f50a7977e65ull, 0xc52dc937487cc197ull, 0xbff445a1c9b19184ull, 0xb2fd8206a0325cb9ull,
+  0xbf32bb70e354a83cull, 0x587313d37681ffc2ull, 0x687c0b98ef7ad731ull, 0x2c09a37608248f89ull,
+  0x20c8d9b1b6e28faaull, 0x747008703cbc9483ull, 0x0bdcacce877cb231ull, 0xf740c0ac1dc79a0cull,
+
+  0x8e843baef228089dull, 0xc379d4c3b6e28c1bull, 0xb5d44eee257f1206ull, 0xb5dfee44ef6b05adull,
+  0xaab0edc0f566f359ull, 0x21883ede514d45ddull, 0xcdaeea0482c73a46ull, 0xb3751631e5645c4full,
+  0xd22ac81254aad1acull, 0x59c0f8f5c93121dfull, 0x3b343a848e3097c3ull, 0x87c65a3a866ed9edull,
+  0xf112c880c63ddafdull, 0xff98b9575bfce057ull, 0xd6e8f6ad6f4da8aeull, 0xba0379ba20645a51ull,
 };
 
 /* Multiply 64-bit V and P and return sum of high and low parts of the result. */
@@ -185,7 +205,6 @@ static _MUM_INLINE uint64_t _mum_le16 (uint16_t v) {
    The macro value affects the result hash for strings > 128 bit. The unroll factor greatly affects
    the hashing speed. We prefer the speed. */
 
-#define _MUM_UNROLL_BLOCK_FACTOR 8
 #if defined(__AVX2__)
 typedef long long __attribute__ ((vector_size (32), aligned (1))) _mum_block_t;
 typedef int __attribute__ ((vector_size (32), aligned (1))) _mum_v8si;
@@ -282,19 +301,20 @@ static _MUM_INLINE uint64_t _mum_fold_block (_mum_block_t *b) {
 }
 #endif
 
-#define _MUM_MIN_BLOCK_ROUNDS 2
-#define _MUM_UNROLL_FACTOR 8
+#define _MUM_UNROLL_BLOCK_FACTOR 16
+#define _MUM_UNROLL_FACTOR 16
 
 static _MUM_INLINE uint64_t _MUM_OPTIMIZE ("unroll-loops")
   _mum_hash_aligned (uint64_t start, const void *key, size_t len) {
   uint64_t result = start;
   const unsigned char *str = (const unsigned char *) key;
-  uint64_t u64;
-  size_t i;
+  uint64_t u64, w;
+  size_t i, j;
   size_t n;
 
   result = _mum (result, _mum_block_start_prime);
-  if (len >= _MUM_MIN_BLOCK_ROUNDS * _MUM_UNROLL_BLOCK_FACTOR * sizeof (_mum_block_t)) {
+  i = 0;
+  if (len >= _MUM_UNROLL_BLOCK_FACTOR * sizeof (_mum_block_t)) {
     _mum_block_t state;
     _mum_zero_block (&state);
     do {
@@ -305,46 +325,81 @@ static _MUM_INLINE uint64_t _MUM_OPTIMIZE ("unroll-loops")
       /* We will use the same prime numbers on the next iterations -- randomize the state. */
       _mum_factor_block (&state, (_mum_block_t *) _mum_unroll_primes);
     } while (len >= _MUM_UNROLL_BLOCK_FACTOR * sizeof (_mum_block_t));
-    result ^= _mum_fold_block (&state);
+    result += _mum_fold_block (&state);
   }
+  static_assert (_MUM_UNROLL_BLOCK_FACTOR <= sizeof (_mum_primes) / sizeof (uint64_t));
+  static_assert (_MUM_UNROLL_BLOCK_FACTOR * sizeof (_mum_block_t) / sizeof (uint64_t) + 7
+                 <= sizeof (_mum_primes) / sizeof (uint64_t));
+  i = 0;
   while (len >= _MUM_UNROLL_FACTOR * sizeof (uint64_t)) {
-    for (i = 0; i < _MUM_UNROLL_FACTOR; i += 2)
+    for (j = 0; j < _MUM_UNROLL_FACTOR; j += 2, i += 2)
       result ^= _mum (_mum_le (((uint64_t *) str)[i]) ^ _mum_primes[i],
                       _mum_le (((uint64_t *) str)[i + 1]) ^ _mum_primes[i + 1]);
     len -= _MUM_UNROLL_FACTOR * sizeof (uint64_t);
-    str += _MUM_UNROLL_FACTOR * sizeof (uint64_t);
-    /* We will use the same prime numbers on the next iterations -- randomize the state. */
-    result = _mum (result, _mum_unroll_primes[0]);
   }
-  n = len / sizeof (uint64_t);
-  for (i = 0; i < n; i++)
-    result ^= _mum (_mum_le (((uint64_t *) str)[i]) + _mum_primes[i], _mum_primes[i]);
+  n = len / sizeof (uint64_t) & ~(size_t) 1;
+  for (j = 0; j < n; j += 2, i += 2)
+    result ^= _mum (_mum_le (((uint64_t *) str)[i]) + _mum_primes[i],
+                    _mum_le (((uint64_t *) str)[i + 1]) + _mum_primes[i + 1]);
   len -= n * sizeof (uint64_t);
-  str += n * sizeof (uint64_t);
+  str += i * sizeof (uint64_t);
   switch (len) {
+  case 15:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 1] + _mum_le32 (*(uint32_t *) (str + 8));
+    u64 += (_mum_le16 (*(uint16_t *) (str + 12)) << 32) + ((uint64_t) str[14] << 48);
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 14:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 2] + _mum_le32 (*(uint32_t *) (str + 8));
+    u64 += _mum_le16 (*(uint16_t *) (str + 12)) << 32;
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 13:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 3] + _mum_le32 (*(uint32_t *) (str + 8));
+    u64 += (uint64_t) str[12] << 32;
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 12:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 4] + _mum_le32 (*(uint32_t *) (str + 8));
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 11:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 5] + _mum_le16 (*(uint16_t *) (str + 8));
+    u64 += (uint64_t) str[10] << 16;
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 10:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 6] + _mum_le16 (*(uint16_t *) (str + 8));
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 9:
+    w = _mum_le (*(uint64_t *) str) + _mum_primes[i];
+    u64 = _mum_primes[i + 7] + str[8];
+    return result ^ _mum (u64 ^ _mum_tail_prime, w);
+  case 8: return result ^ _mum (_mum_le (*(uint64_t *) str) + _mum_primes[i], _mum_primes[i]);
   case 7:
-    u64 = _mum_primes[0] + _mum_le32 (*(uint32_t *) str);
+    u64 = _mum_primes[i + 1] + _mum_le32 (*(uint32_t *) str);
     u64 += (_mum_le16 (*(uint16_t *) (str + 4)) << 32) + ((uint64_t) str[6] << 48);
     return result ^ _mum (u64, _mum_tail_prime);
   case 6:
-    u64 = _mum_primes[1] + _mum_le32 (*(uint32_t *) str);
+    u64 = _mum_primes[i + 2] + _mum_le32 (*(uint32_t *) str);
     u64 += _mum_le16 (*(uint16_t *) (str + 4)) << 32;
     return result ^ _mum (u64, _mum_tail_prime);
   case 5:
-    u64 = _mum_primes[2] + _mum_le32 (*(uint32_t *) str);
+    u64 = _mum_primes[i + 3] + _mum_le32 (*(uint32_t *) str);
     u64 += (uint64_t) str[4] << 32;
     return result ^ _mum (u64, _mum_tail_prime);
   case 4:
-    u64 = _mum_primes[3] + _mum_le32 (*(uint32_t *) str);
+    u64 = _mum_primes[i + 4] + _mum_le32 (*(uint32_t *) str);
     return result ^ _mum (u64, _mum_tail_prime);
   case 3:
-    u64 = _mum_primes[4] + _mum_le16 (*(uint16_t *) str);
+    u64 = _mum_primes[i + 5] + _mum_le16 (*(uint16_t *) str);
     u64 += (uint64_t) str[2] << 16;
     return result ^ _mum (u64, _mum_tail_prime);
   case 2:
-    u64 = _mum_primes[5] + _mum_le16 (*(uint16_t *) str);
+    u64 = _mum_primes[i + 6] + _mum_le16 (*(uint16_t *) str);
     return result ^ _mum (u64, _mum_tail_prime);
-  case 1: u64 = _mum_primes[6] + str[0]; return result ^ _mum (u64, _mum_tail_prime);
+  case 1: u64 = _mum_primes[i + 7] + str[0]; return result ^ _mum (u64, _mum_tail_prime);
   }
   return result;
 }
@@ -440,7 +495,7 @@ static _MUM_INLINE uint64_t mum_hash_finish (uint64_t h) { return _mum_final (h)
 
 /* Fast hashing of KEY with SEED. The hash is always the same for the same key on any target. */
 static _MUM_INLINE size_t mum_hash64 (uint64_t key, uint64_t seed) {
-  return mum_hash_finish (mum_hash_step (mum_hash_init (seed), key));
+  return mum_hash_finish (mum_hash_step (mum_hash_init (seed + 8), key));
 }
 
 /* Hash data KEY of length LEN and SEED. The hash depends on the target endianess and the unroll
